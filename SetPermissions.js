aeb92f00ee9af6e40dc0c0d0c26de21e978e8e84
PradeepRN1871991 <pradeep1871991@gmail.com>
2018-07-26 21:57:31 +0530

    update

--- SetPermissions.js
+++ SetPermissions.js
@@ -7,7 +7,7 @@ if(items.length>0)
     for(var i=0; i<=items.length;i++)
             {
                 var itemid=items[i].id;
-                var siterestapiurl=_spPageContextInfo.webAbsoluteUrl+"/_api/web/lists(guid'64F4842C-94C4-433F-ACE7-0DC29E35AA90')/items(itemid)/roleassignments/removeroleassignment(principalid=6,roleDefId=1073741829)";
+                var siterestapiurl=_spPageContextInfo.webAbsoluteUrl+"/_api/web/lists(guid'')/items(itemid)/roleassignments/removeroleassignment(principalid=6,roleDefId=1073741829)";
                 var headers = {  
                     "Accept": "application/json;odata=verbose",  
                     "content-Type": "application/json;odata=verbose",  
